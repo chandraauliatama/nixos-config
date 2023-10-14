3382d9bfc77b58f792e8d5beca07c6fc20d39b81
chandraauliatama <chandraaulia4@gmail.com>
2023-10-15 01:18:55 +0700

    Initial Commit: Success setup flake and home manager

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "A very basic flake";
+
+  nix.settings.trusted-users = [ "chandraat" ]; 
+
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      "https://cache.nixos.org/"
+    ];
+
+    extra-substituters = [
+      # Nix community's cache server
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+
+      # Run the following command in the flake's directory to
+      # deploy this configuration on any NixOS system:
+      #  sudo nixos-rebuild switch --flake .#nixos
+      "nixos" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+	modules = [
+          # Import the configuration.nix here, so that the
+          # old configuration file can still take effect.
+          # Note: configuration.nix itself is also a Nix Module,
+          ./configuration.nix
+
+	    # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.chandraat = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
+    };
+  };
+}
